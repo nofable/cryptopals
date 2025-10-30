8708f3a914c56a8d6210070b77ec65c3995663a2
William Woodhead <6097010+nofable@users.noreply.github.com>
2025-10-30 20:53:49 +0000

    tidied up code for chapter 1.3

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,25 @@ mod hex_to_base64;
 mod xor;
 use hex_to_base64::*;
 use xor::*;
+
 fn main() {
     println!("Hello, world!");
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_1_2() {
+        let treatment = "1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736";
+        let chars: Vec<u8> = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+            .as_bytes()
+            .to_vec();
+        let decoded_treatment = hex::decode(treatment).unwrap();
+        for c in chars {
+            let result = xor_bytes(&vec![c], &decoded_treatment);
+            println!("{:?}", result);
+        }
+    }
 }
--- src/xor.rs
+++ src/xor.rs
@@ -1,17 +1,23 @@
+use std::cmp::max;
+
 use base64::prelude::*;
 
-fn xor_bytes(a: Vec<u8>, b: Vec<u8>) -> Vec<u8> {
-    assert_eq!(a.len(), b.len());
-    a.iter().zip(b.iter()).map(|(i, j)| i ^ j).collect()
+pub fn xor_bytes(a: &Vec<u8>, b: &Vec<u8>) -> Vec<u8> {
+    let l = max(a.len(), b.len());
+    let mut ap: Vec<u8> = vec![0u8; l - a.len()];
+    ap.extend_from_slice(a);
+    let mut bp: Vec<u8> = vec![0u8; l - b.len()];
+    bp.extend_from_slice(b);
+    ap.iter().zip(bp.iter()).map(|(i, j)| i ^ j).collect()
 }
 
-fn xor_str(a: &str, b: &str) -> String {
+pub fn xor_hex_str(a: &str, b: &str) -> String {
+    assert_eq!(a.len(), b.len());
     let decoded_a = hex::decode(a).unwrap();
     let decoded_b = hex::decode(b).unwrap();
-    let output_bytes = xor_bytes(decoded_a, decoded_b);
+    let output_bytes = xor_bytes(&decoded_a, &decoded_b);
     hex::encode(output_bytes)
 }
-
 
 #[cfg(test)]
 mod tests {
@@ -21,8 +27,8 @@ mod tests {
     fn test_success_case() {
         let input_a = "1c0111001f010100061a024b53535009181c";
         let input_b = "686974207468652062756c6c277320657965";
-        let expected : String = "746865206b696420646f6e277420706c6179".into();
-        assert_eq!(xor_str(input_a, input_b), expected);
+        let expected: String = "746865206b696420646f6e277420706c6179".into();
+        assert_eq!(xor_hex_str(input_a, input_b), expected);
     }
 
     #[test]
@@ -30,7 +36,7 @@ mod tests {
     fn test_fail_mismatch_len() {
         let input_a = "11001f010100061a024b53535009181c";
         let input_b = "686974207468652062756c6c277320657965";
-        xor_str(input_a, input_b);
+        xor_hex_str(input_a, input_b);
     }
 
     #[test]
@@ -38,7 +44,6 @@ mod tests {
     fn test_fail_not_hex() {
         let input_a = "$%&111001f010100061a024b53535009181c";
         let input_b = "686974207468652062756c6c277320657965";
-        xor_str(input_a, input_b);
+        xor_hex_str(input_a, input_b);
     }
-
 }
