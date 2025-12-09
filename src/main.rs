51c65b4db590e3931e49b538e5f7cf716fd169ff
William Woodhead <6097010+nofable@users.noreply.github.com>
2025-12-09 09:01:31 +0000

    hacky commit - introduced clap command line and got vigenere running from command line

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,97 @@
-use std::collections::{HashMap, HashSet};
+use base64::{Engine, prelude::BASE64_STANDARD};
+use std::fs;
 mod hamming;
 mod hex_to_base64;
 mod transposer;
 mod xor;
+use clap::Parser;
 use hamming::*;
 use hex_to_base64::*;
 use transposer::*;
 use xor::*;
 
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    #[arg(short, long)]
+    input_file: String,
+
+    #[arg(short, long, default_value_t = 40)]
+    max_hamming: usize,
+
+    #[arg(short = 'a', long, default_value_t = 4)]
+    hamming_sample_size: usize,
+
+    #[arg(short, long, default_value_t = 3)]
+    no_of_results: usize,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+    vigenere(
+        args.input_file,
+        args.max_hamming,
+        args.hamming_sample_size,
+        args.no_of_results,
+    );
+}
+
+fn vigenere(
+    input_file: String,
+    max_hamming: usize,
+    hamming_sample_size: usize,
+    no_of_results: usize,
+) {
+    let content = fs::read_to_string(input_file).unwrap().replace("\n", "");
+    let decoded = BASE64_STANDARD.decode(content).unwrap();
+    let mut results: Vec<(f64, usize)> = Vec::new();
+    // guess at the key size by evaluating the lowest hamming_distance, normalized by keysize
+    for i in 2..max_hamming {
+        if decoded.len() > i * hamming_sample_size {
+            let first = &decoded[0..i];
+            let second = &decoded[i..(i * 2)];
+            let third = &decoded[(i * 2)..(i * 3)];
+            let fourth = &decoded[(i * 3)..(i * 4)];
+            let distance1 = hamming_distance(first, second);
+            let distance2 = hamming_distance(second, third);
+            let distance3 = hamming_distance(third, fourth);
+            let normalized1 = distance1 as f64 / i as f64;
+            let normalized2 = distance2 as f64 / i as f64;
+            let normalized3 = distance3 as f64 / i as f64;
+            let average = (normalized1 + normalized2 + normalized3) / 3.0;
+            results.push((average, i));
+        }
+    }
+    results.sort_by(|a, b| a.0.total_cmp(&b.0));
+    // iterate through the top results
+    for (_, keysize) in &results[0..no_of_results] {
+        let blocks: Vec<Vec<u8>> = decoded
+            .clone()
+            .chunks(*keysize)
+            .map(|block| block.to_vec())
+            .collect();
+
+        // transpose blocks
+        let transposed = transpose(blocks);
+        let mut key_chars: Vec<char> = Vec::new();
+        // single char XOR
+        for tblock in transposed {
+            // histogram for letter frequency
+            let mut scx_results = decode_single_character_xor(&tblock);
+            scx_results.sort_by(|a, b| a.0.total_cmp(&b.0));
+            let top_result = scx_results.first().unwrap();
+            key_chars.push(top_result.1);
+        }
+        let key_string: String = key_chars.iter().collect();
+        println!("final key is '{}'", key_string);
+        let final_result =
+            repeating_key_xor_strs(&String::from_utf8(decoded.clone()).unwrap(), &key_string);
+        println!("Final result is {}", final_result);
+    }
 }
 
 #[cfg(test)]
 mod tests {
-    use std::fs;
-
-    use base64::{Engine, prelude::BASE64_STANDARD};
-
     use super::*;
 
     #[test]
